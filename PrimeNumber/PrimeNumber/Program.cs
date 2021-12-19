4b6156417140c178a28117871bfb7419aa31af1c
Shubham Seth <shubhamsth1234@gmail.com>
2021-12-19 14:38:32 +0530

    Added PrimeNumber and Reverse a number prog

---
+++ PrimeNumber/PrimeNumber/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+/*
+Prime Number
+Just like the Perfect number, the Prime number is also a special type of number. When
+the number is divided greater than 1 and divided by 1 or itself is referred to as the Prime
+number. 0 and 1 are not counted as prime numbers. All the even numbers can be
+divided by 2, so 2 is the only even prime number.
+ */
+
+namespace PrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a Number : ");
+            int number = int.Parse(Console.ReadLine());
+
+            bool IsPrime = true;
+            for (int i = 2; i < number / 2; i++)
+            {
+                if (number % i == 0)
+                {
+                    IsPrime = false;
+                    break;
+                }
+            }
+            if (IsPrime)
+            {
+                Console.WriteLine("{0} is a Prime Number.", number);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a Prime Number.", number);
+            }
+        }
+    }
+}
---
+++ Reverse_A_Number/Reverse_A_Number/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+/*
+Reverse a number
+In C#, we can reverse a number either by using for loop, while loop, or using recursion.
+The simplest way to reverse a number is by using for loop or while loop. In order to
+reverse a number, we have to follow the following steps:
+a. We need to calculate the remainder of the number using the modulo
+b. After that, we need to multiply the variable reverse by 10 and add the remainder into
+it.
+c. We then divide the number by 10 and repeat steps until the number becomes 0.
+ */
+
+namespace Reverse_A_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a Number : ");
+            int number = int.Parse(Console.ReadLine());
+            int reminder, reverse = 0;
+
+            while (number > 0)
+            {
+                //Get the remainder by dividing the number with 10  
+                reminder = number % 10;
+                //multiply the sum with 10 and then add the reminder
+                reverse = (reverse * 10) + reminder;
+                //Get the quotient by dividing the number with 10 
+                number = number / 10;
+            }
+            Console.WriteLine($"The Reverse order is : {reverse}");
+            Console.ReadKey();
+        }
+    }
+}
